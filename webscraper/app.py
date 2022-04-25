15c8433cfec477934bb799f1457718b0e6d1d0fc
Balazs <bacs.balazs@yahoo.com>
2022-04-26 00:13:43 +0300

    small updates

--- webscraper/app.py
+++ webscraper/app.py
@@ -6,20 +6,26 @@ source = requests.get('https://maszol.ro/').text
 
 soup = BeautifulSoup(source, 'html.parser')
 
-articles = soup.find_all('article')
+articles = soup.find_all('article')[0:5]
 
-kulcsszavak = ['háborúban', 'bomba', 'konfliktus', 'katonai', 'terror', 'csökken']
+kulcsszavak = ['háború', 'bomba', 'konfliktus', 'katona', 'terror', 'kigyulladt', 'villám', 'vihar']
+checks = []
 
-print( any(word in 'robbant egy észak-afganisztáni szunnita terror'.lower() for word in kulcsszavak) )
+for article in articles:
+     h2=', '.join([x.get_text() for x in article.find_all('h2')])
+     if any(word in h2 for word in kulcsszavak):
+         checks.append('-')
+     else:
+         checks.append('+')
+# for art in articles:
+#     print(type(art))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-    #print(len(cikkek))
    
-    return render_template('index.html', articles=articles, kulcsszavak=kulcsszavak)
+    return render_template('index.html', len=len, articles=articles, kulcsszavak=kulcsszavak, checks=checks)
 
 if __name__ == "__main__":
     app.run(debug=True)
