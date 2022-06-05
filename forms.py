dfdff1d35349623603ecfb69f128349ed7460a31
Balazs <bacs.balazs@yahoo.com>
2022-06-05 20:18:15 +0300

    small fixes

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, HiddenField, SubmitField
-
-class HiddenForm(FlaskForm):
-    article = HiddenField('Article')
-    
-    submit = SubmitField('Submit')
